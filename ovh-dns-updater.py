719576dc53b5d286a75fbfe68cc7ae8494fe2496
Daniel Beltejar <danielbeltejar@gmail.com>
2023-12-26 20:47:05 +0100

    feat: migrated to microservice

--- ovh-dns-updater.py
+++ ovh-dns-updater.py
@@ -1,217 +1,185 @@
-#!/usr/bin/env python
-# -*- encoding: utf-8 -*-
-
-
 import json
+import os
 import ovh
-import sys
-import requests, time
+import requests
+import time
 from kubernetes import client, config
 
-'''
-API credentials
-can be provided in variety of ways:
-(see https://github.com/ovh/python-ovh#configuration)
--explicitly here :
-'''
-#client = ovh.Client(
-#        endpoint           = "ovh-eu",
-#        application_key    = "XXXXXXXXXXXXXXXX",
-#        application_secret = "YYYYYYYYYYYYYYYY",
-#        consumer_key       = "ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ"
-#        )
-'''
--or in the ENVIRONMENT 
--or in a ovh.conf file
-in which cases we can call with no argument (the ovh module gets the credentials on its own):
-'''
-client = ovh.Client()
 
-# Should missing IP address being considered as error (Depend on the ISP, internet box settings...)?
-# In case a required IP cannot be obtained, the script will send an email and stop without updating anything. 
-# If, for any reason, IPv4 or IPv6 address cannot be obtained and if it is not protected by this list, the corresponding records will be deleted for all hosts.
-ip_versions_required = [4] # MUST not be empty. Can be [4],[6] or [4,6] 
+def get_ovh_credentials():
+    """
+    Retrieve OVH API credentials from environment variables.
+    """
+    endpoint = os.environ.get("OVH_ENDPOINT", "ovh-eu")
+    application_key = os.environ.get("OVH_APPLICATION_KEY", "")
+    application_secret = os.environ.get("OVH_APPLICATION_SECRET", "")
+    consumer_key = os.environ.get("OVH_CONSUMER_KEY", "")
 
-default_ttl =  600  # seconds
-# ttl = how long will a DNS server cache the value before checking it at the Registrar. Longer value yields faster name resolution most of the time, but less frequent updates
-
-# Load in-cluster configuration to access ConfigMap
-config.load_incluster_config()
-v1 = client.CoreV1Api()
-
-# Fetch the ConfigMap data
-config_map = v1.read_namespaced_config_map("ovh-dns-config", "namespace-name")
-hosts_json = config_map.data["hosts.json"]
-hosts = json.loads(hosts_json)
-
-checkDNS_interval_hrs = 12.1 # when the saved IP addresses are old, check the DNS record, even if the addresses did not change
-#save last known address in local file.
-current_ip_file = "/tmp/current_ip.json"
+    return {
+        "endpoint": endpoint,
+        "application_key": application_key,
+        "application_secret": application_secret,
+        "consumer_key": consumer_key
+    }
 
 
-def send_email(msg, sender = 'no_reply@mydomain.com', receiver = 'admin@mydomain.com') :
-  import smtplib
+def get_namespace():
+    """
+    Retrieve the namespace from the service account token mounted in the pod.
+    """
+    namespace_path = '/var/run/secrets/kubernetes.io/serviceaccount/namespace'
+    with open(namespace_path, 'r') as f:
+        namespace = f.read().strip()
+    return namespace
 
-  try :
-     smtpObj = smtplib.SMTP('localhost')
-     smtpObj.sendmail(sender, receiver,
-         "From: {}\nTo: {}\nSubject: DNS update problem\n\nThe ovh-dns-updater.py script reports:\n{}\n".format(sender, receiver, msg)
-         )
-  except smtplib.SMTPException:
-     print( timestamp()," : Error unable to send email")
 
-def get_current_ip(v = 4):
-    if v == 4 :
+def get_config_map():
+    """
+    Load in-cluster configuration and fetch ConfigMap data.
+    """
+    config.load_incluster_config()
+    namespace = get_namespace()
+    v1 = client.CoreV1Api()
+    config_map_name = "ovh-dns-config"
+    config_map = v1.read_namespaced_config_map(config_map_name, namespace)
+    hosts_json = config_map.data.get("hosts.json", "[]")
+    return json.loads(hosts_json)
+
+
+def get_current_ip(v=4):
+    """
+    Retrieve the current IPv4 or IPv6 address.
+    """
+    if v == 4:
         url_list = ["https://api.ipify.org", "https://ipv4.lafibre.info/ip.php", "https://v4.ident.me"]
-    else :
+    else:
         url_list = ["https://api6.ipify.org", "https://ipv6.lafibre.info/ip.php", "https://v6.ident.me"]
     ip = ""
     message = ""
-    for url in url_list :
-        try :
+    for url in url_list:
+        try:
             r = requests.get(url, timeout=30.0)
         except requests.exceptions.RequestException as e:
-            message += "failed getting ipv{} address from {} because {} occurred\n".format(v, url, str(e))
+            message += f"failed getting ipv{v} address from {url} because {str(e)} occurred\n"
             continue
-        if r.status_code == requests.codes.ok :
+        if r.status_code == requests.codes.ok:
             ip = r.text
             break
-        else : 
-            message += "{} : Cannot get IPv{} from {}: requests.get returned status_code {}.\n".format(timestamp(), v, url, r.status_code)
+        else:
+            message += f"{time.asctime(time.localtime(time.time()))} : Cannot get IPv{v} from {url}: " \
+                       f"requests.get returned status_code {r.status_code}.\n"
     if ip != "":
         return ip
-    elif v in ip_versions_required :
-            message += "Failed getting required IPv{}. There is most likely a real connectivity problem. Aborting". format( v)
-            print(message)
-            send_email (message)
-            quit()
-    else :
+    elif v in [4]:  # IPv4 is required
+        message += f"Failed getting required IPv{v}. There is most likely a real connectivity problem. Aborting"
+        print(message)
+        quit()
+    else:
         return False
 
-def timestamp() :
-    return time.asctime( time.localtime(time.time()) )
 
-def update_record(domain, subdomain, new_ip, _ttl = 600):
-    #Update the (A or AAAA) record with the provided IP
-
+def update_record(domain, subdomain, new_ip, _ttl=600):
+    """
+    Update the (A or AAAA) record with the provided IP.
+    """
     global records_changed
     typ = 'AAAA' if ":" in new_ip else 'A'
-    #print("checking record {} for {}.{}".format(typ,subdomain,domain))
-    path = "/domain/zone/{}/record".format(domain)
-    result = client.get(path,
-                        fieldType = typ,
-                        subDomain = subdomain
-                     )
+    path = f"/domain/zone/{domain}/record"
+    result = client.get(path, fieldType=typ, subDomain=subdomain)
 
     if len(result) != 1:
-        #creating NEW record
-        result = client.post(path,
-                    fieldType = typ,
-                    subDomain = subdomain,
-                    target = new_ip,
-                    ttl = _ttl
-                     )
-        client.post('/domain/zone/{}/refresh'.format(domain))
-        result = client.get(path,
-                        fieldType=typ,
-                        subDomain=subdomain
-                        )
+        result = client.post(path, fieldType=typ, subDomain=subdomain, target=new_ip, ttl=_ttl)
+        client.post(f"/domain/zone/{domain}/refresh")
+        result = client.get(path, fieldType=typ, subDomain=subdomain)
         record_id = result[0]
         records_changed += 1
-        print("{} : ### created new record {} for {}.{}".format(timestamp(),typ,subdomain,domain))
-    else :
-        # record exists
+        print(f"{time.asctime(time.localtime(time.time()))} : ### created new record {typ} for {subdomain}.{domain}")
+    else:
         record_id = result[0]
-        path = "/domain/zone/{}/record/{}".format(domain,record_id)
+        path = f"/domain/zone/{domain}/record/{record_id}"
         result = client.get(path)
         oldip = result['target']
-        #print('record exists, with ip :',oldip)
-        if oldip == new_ip :
-            #print('nothing to do')
+        if oldip == new_ip:
             return
-        else :
-            #print('updating to ', new_ip)
-            result = client.put(path,
-                subDomain = subdomain,
-                target = new_ip,
-                ttl = _ttl
-                 )
-            client.post('/domain/zone/{}/refresh'.format(domain))
+        else:
+            result = client.put(path, subDomain=subdomain, target=new_ip, ttl=_ttl)
+            client.post(f"/domain/zone/{domain}/refresh")
             records_changed += 1
-    #checking changes
-    result = client.get("/domain/zone/{}/record/{}".format(domain,record_id))
-    if new_ip != result['target'] :
-        records_changed -= 1
-        raise Exception("Error updating {}.{} with {}".format(subdomain,domain,new_ip))
 
 
 def delete_record(domain, subdomain, typ):
     """
-    if it exists, delete an A or AAAA record
-    (because the corresponding IP is not available)
+    Delete an A or AAAA record if it exists.
     """
-    #print("checking record {} for {}.{}".format(typ,subdomain,domain))
     global records_changed
-    result = client.get("/domain/zone/{}/record".format(domain),
-                        fieldType = typ,
-                        subDomain = subdomain
-                     )
+    result = client.get(f"/domain/zone/{domain}/record", fieldType=typ, subDomain=subdomain)
     if len(result) == 1:
-        # record exists, delete it
         record_id = result[0]
-        print("{} : ### deleting record {} for {}.{}".format(timestamp(),typ,subdomain,domain))
-        client.delete("/domain/zone/{}/record/{}".format(domain,record_id))
-        client.post('/domain/zone/{}/refresh'.format(domain))
+        print(f"{time.asctime(time.localtime(time.time()))} : ### deleting record {typ} for {subdomain}.{domain}")
+        client.delete(f"/domain/zone/{domain}/record/{record_id}")
+        client.post(f"/domain/zone/{domain}/refresh")
         records_changed += 1
 
 
+def timestamp():
+    """
+    Get the current timestamp.
+    """
+    return time.asctime(time.localtime(time.time()))
+
+
+# Main script logic
+credentials = get_ovh_credentials()
+client = ovh.Client(**credentials)
+
+# Use 'get_config_map()' to retrieve the hosts list
+hosts = get_config_map()
+
+checkDNS_interval_hrs = 12.1
+current_ip_file = "/tmp/current_ip.json"
+
+# Retrieve current IPv4 and IPv6
 current_ipv4 = get_current_ip(4)
 current_ipv6 = get_current_ip(6)
-#print('current ips: {} ; {}'.format(current_ipv4, current_ipv6))
 
-#reload saved values & compare
+# Reload saved values & compare
 try:
     with open(current_ip_file, 'r') as f:
-        old_time, old_ipv4, old_ipv6   = json.load(f)
-    need_update = (old_ipv4 != current_ipv4) or (old_ipv6 != current_ipv6) or ((old_time - time.time()) > 3600.0 * checkDNS_interval_hrs)
+        old_time, old_ipv4, old_ipv6 = json.load(f)
+    need_update = (old_ipv4 != current_ipv4) or (old_ipv6 != current_ipv6) or (
+            (old_time - time.time()) > 3600.0 * checkDNS_interval_hrs)
 except IOError:
-    #print("No old ips recorded")
     need_update = True
-if need_update :
-  records_changed = 0
-  try :
-    for host in hosts :
-      domain = host["domain"]
-      subdomain = host ["subdomain"]
-      if ('ipv4' not in host) or (host['ipv4'] != False) :
-          if current_ipv4 :
-              ttl = default_ttl if ('ttl' not in host) else host['ttl']
-              update_record(domain, subdomain, current_ipv4, _ttl = ttl)
-          else :
-              delete_record(domain, subdomain, 'A')
-      else :
-          #print("Not touching A record for {}.{}, as instructed".format(subdomain, domain))
-          pass
-      if ('ipv6' not in host) or (host['ipv6'] != False) :
-          if current_ipv6 :
-              ttl = default_ttl if ('ttl' not in host) else host['ttl']
-              update_record(domain, subdomain, current_ipv6, _ttl = ttl)
-          else :
-              delete_record(domain, subdomain, 'AAAA')
-      else :
-          #print("Not touching AAAA record for {}.{}, as instructed".format(subdomain, domain))
-          pass
-    #all hosts records have been updated without errors, log change and save current addresses
-    print("{} : new addresses {} ; {} -- {} records updates".format(timestamp(), current_ipv4, current_ipv6, records_changed))
-    with open(current_ip_file, 'w') as f:
-      json.dump([time.time(), current_ipv4, current_ipv6],f)
-  except Exception as e: #some error occured (API down, keys expired...?),
-    msg = "{} : ### error {} while updating records!! {} records updated with new addresses {} ; {}".format(timestamp(),  str(e), records_changed, current_ipv4, current_ipv6)
-    print(msg)
-    send_email(msg)
-    # not saving new addresses, so that update is attempted again.
-else :
-  #print("nothing to do!")
-  pass
 
-
+if need_update:
+    records_changed = 0
+    default_ttl = 600
+    try:
+        for host in hosts:
+            domain = host["domain"]
+            subdomain = host["subdomain"]
+            if ('ipv4' not in host) or (host['ipv4'] != False):
+                if current_ipv4:
+                    ttl = default_ttl if ('ttl' not in host) else host['ttl']
+                    update_record(domain, subdomain, current_ipv4, _ttl=ttl)
+                else:
+                    delete_record(domain, subdomain, 'A')
+            else:
+                pass
+            if ('ipv6' not in host) or (host['ipv6'] != False):
+                if current_ipv6:
+                    ttl = default_ttl if ('ttl' not in host) else host['ttl']
+                    update_record(domain, subdomain, current_ipv6, _ttl=ttl)
+                else:
+                    delete_record(domain, subdomain, 'AAAA')
+            else:
+                pass
+        print(f"{timestamp()} : new addresses {current_ipv4} ; {current_ipv6} -- {records_changed} records updates")
+        with open(current_ip_file, 'w') as f:
+            json.dump([time.time(), current_ipv4, current_ipv6], f)
+    except Exception as e:
+        msg = f"{timestamp()} : ### error {str(e)} while updating records!! {records_changed} records updated " \
+              f"with new addresses {current_ipv4} ; {current_ipv6}"
+        print(msg)
+else:
+    pass
